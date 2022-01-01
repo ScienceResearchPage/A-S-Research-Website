4590e978593130db4ba1b95ef5a3de8ed09028d7
Anoop Singh <anoopsingh@anoops-air.lan>
2021-12-31 19:42:55 -0500

    all getters and setters for first two tables

--- database.py
+++ database.py
@@ -12,17 +12,74 @@ cur = conn.cursor()
 # print(db_version)
 
 
-#Student
-def add_student(name):
-    query = "INSERT INTO student_attributes (student_name) VALUES (%s)"
-    cur.execute(query, [name])
-    conn.commit()
+#------------------Student_attributes-----------------------#
+
+#Getters
+def get_email(name):
+    query = 'SELECT email FROM student_attributes WHERE student_name = \'' + name + '\''
+    cur.execute(query)
+    exec = cur.fetchone()
+    return exec[0]
+
+def get_necessary_minutes(name):
+    query = 'SELECT necessary_minutes FROM student_attributes WHERE student_name = \'' + name + '\''
+    cur.execute(query)
+    exec = cur.fetchone()
+    return exec[0]
+
+def get_unconfirmed_minutes(name):
+    query = 'SELECT unconfirmed_minutes FROM student_attributes WHERE student_name = \'' + name + '\''
+    cur.execute(query)
+    exec = cur.fetchone()
+    return exec[0]
+
+def get_confirmed_minutes(name):
+    query = 'SELECT confirmed_minutes FROM student_attributes WHERE student_name = \'' + name + '\''
+    cur.execute(query)
+    exec = cur.fetchone()
+    return exec[0]
+
+def get_status(name):
+    query = 'SELECT exempt FROM student_attributes WHERE student_name = \'' + name + '\''
+    cur.execute(query)
+    exec = cur.fetchone()
+    return exec[0]
 
 def student_list():
     query = 'SELECT student_name FROM student_attributes'
     cur.execute(query)
     exec = cur.fetchall()
     return list(map(lambda x: x[0], exec))
+
+#Setters
+def add_student(name):
+    query = "INSERT INTO student_attributes (student_name) VALUES (%s)"
+    cur.execute(query, [name])
+    conn.commit()
+
+def change_name_from_name(old_name, new_name):
+    query = 'UPDATE student_attributes SET student_name = \'' + new_name + '\' WHERE student_name = \'' + old_name + '\''
+    cur.execute(query)
+    conn.commit()
+
+def change_email_from_name(name, email):
+    query = 'UPDATE student_attributes SET email = \'' + email + '\' WHERE student_name = \'' + name + '\''
+    cur.execute(query)
+    conn.commit()
+
+def toggle_exempt_from_name(name):
+    query = 'UPDATE student_attributes SET exempt = NOT exempt WHERE student_name = \'' + name + '\''
+    cur.execute(query)
+    conn.commit()
+
+def change_necessary_minutes_from_name(name, minutes):
+    query = 'UPDATE student_attributes SET necessary_minutes = ' + str(minutes) + ' WHERE student_name = \'' + name + '\''
+    cur.execute(query)
+    conn.commit()
+
+
+
+
 
 #Login
 def get_password():
@@ -36,4 +93,33 @@ def update_password(password):
     cur.execute(query)
     conn.commit()
 
-update_password("test")
+
+
+
+#------------------student_fairs-----------------------#
+#Getters
+def fairs_list():
+    query = 'SELECT fair FROM student_fairs'
+    cur.execute(query)
+    exec = cur.fetchall()
+    return list(map(lambda x: x[0], exec))
+
+def students_from_fair(fair):
+    query = 'SELECT student_name FROM student_fairs WHERE fair = \'' + fair + '\''
+    cur.execute(query)
+    exec = cur.fetchall()
+    return list(map(lambda x: x[0], exec))
+
+#Setters
+def add_student_to_fair(name, fair):
+    query = "INSERT INTO student_fairs (student_name, fair) VALUES (%s, %s)"
+    cur.execute(query, (name, fair))
+    conn.commit()
+
+def delete_student_fair(name, fair):
+    query = "DELETE FROM student_fairs WHERE student_name = \'" + name + '\' AND fair = \'' + fair + '\''
+    cur.execute(query)
+    conn.commit()
+
+
+delete_student_fair("ishaan", "wac")
